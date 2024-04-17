46e099bfca83efe3d0a414943077128b4757dfe4
MohdZaidRapid <www.mohdzaid2902@gmail.com>
2024-04-18 01:55:30 +0530

    added accetrnity

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,13 +12,16 @@ function Navbar({ className }: { className?: string }) {
       className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
     >
       <Menu setActive={setActive}>
-        <Link href={"#"}>
-          <MenuItem
-            setActive={setActive}
-            active={active}
-            item="Home"
-          ></MenuItem>
+        <Link href={"/"}>
+          <MenuItem setActive={setActive} active={active} item="Home">
+            Test
+          </MenuItem>
         </Link>
+        <MenuItem setActive={setActive} active={active} item="Our Courses">
+          <div className="flex flex-col space-y-4">
+            <HoveredLink href="/courses">All courses</HoveredLink>
+          </div>
+        </MenuItem>
       </Menu>
     </div>
   );
