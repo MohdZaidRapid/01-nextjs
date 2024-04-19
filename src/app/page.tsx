89e29b4494fd6863edfc8d0c44accf58af8f71f1
MohdZaidRapid <www.mohdzaid2902@gmail.com>
2024-04-19 23:27:32 +0530

    added accetrnity

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased  
        bg-grid-white/[0.02]">
-      <h1 className="text-2xl text-center">hello,world</h1>;
+      <h1 className="text-2xl text-center text-white">hello,world</h1>;
     </main>
   );
 }
