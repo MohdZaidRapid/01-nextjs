e0cff7d2ce870ffeb742338644b33d725fd15414
MohdZaidRapid <www.mohdzaid2902@gmail.com>
2024-04-23 17:09:04 +0530

    added accetrnity

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import React from "react";
 import { Spotlight } from "./ui/Spotlight";
+import { Button } from "./ui/moving-border";
 
 function HeroSection() {
   return (
@@ -19,7 +20,14 @@ function HeroSection() {
           true skills,join us to unlock your true potential.
         </p>
         <div className="mt-4">
-          <Link href={"/courses"}>Explore courses</Link>
+          <Link href={"/courses"}>
+            <Button
+              borderRadius="1.7rem"
+              className="bg-white dark:bg-black text-black dark:text-white border-neutral-200 dark:border-slate-800"
+            >
+              Explore courses
+            </Button>
+          </Link>
         </div>
       </div>
     </div>
