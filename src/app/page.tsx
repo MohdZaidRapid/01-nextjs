2ab63c160ba860a7bca8882dd7b8c2575ec5a531
MohdZaidRapid <www.mohdzaid2902@gmail.com>
2024-04-23 16:54:24 +0530

    added accetrnity

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
+import HeroSection from "@/components/HeroSection";
+
 export default function Home() {
   return (
     <main
       className="min-h-screen bg-black/[0.96] antialiased  
        bg-grid-white/[0.02]"
     >
-      <h1 className="text-3xl text-center text-black  text-clip">
-        hello,world
-      </h1>
-      ;
+      <HeroSection />
     </main>
   );
 }
